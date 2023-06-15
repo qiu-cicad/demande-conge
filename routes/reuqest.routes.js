6aecdf58430d9b9e8277903eafc51de4beddbdf0
Shengwei LI <lishengwei90@gmail.com>
2023-06-15 17:28:35 +0200

    first fonctional version of web form

--- routes/reuqest.routes.js
+++ routes/reuqest.routes.js
@@ -3,8 +3,12 @@ const Request = require("../models/Request.model");
 const User = require("../models/User.model");
 const differenceInBusinessDays = require("date-fns/differenceInBusinessDays");
 const fullNameToEmail = require("../utils/fullNameToEmail");
+const axios = require("axios").default;
 
 router.post("/send-request", (req, res, next) => {
+  //console.log(req.body)
+  
+  
   const {
     title,
     type,
@@ -14,19 +18,22 @@ router.post("/send-request", (req, res, next) => {
     endDate,
     endInTheMorning,
     endInTheAfternoon,
-    requester,
+    requestor,
     sub,
     validators,
     comments,
   } = req.body;
 
+  //console.log("validators:", validators);
+
   //translate the form data into request properties.
   let startWholeDay = startInTheMorning ? true : false;
   let endWholeDay = endInTheAfternoon ? true : false;
+
   // verify full names are all legal:
   const fullNameRegex = /^([A-Za-z]+\s[A-Za-z]+)(;\s[A-Za-z]+\s[A-Za-z]+)*$/;
   if (
-    fullNameRegex.test(requester) && sub
+    fullNameRegex.test(requestor) && sub
       ? fullNameRegex.test(sub)
       : true === false
   ) {
@@ -42,22 +49,35 @@ router.post("/send-request", (req, res, next) => {
     });
   }
 
-  let requesterEmail = fullNameToEmail(requester);
-  let emailList = [requesterEmail];
+  let requestorEmail = fullNameToEmail(requestor);
+  let emailList = [requestorEmail];
+  //console.log("emailList:", emailList)
   let subEmail = "";
   if (sub) {
     subEmail = fullNameToEmail(sub);
     emailList.push(subEmail);
   }
-  
+
   let validatorsEmail = "";
-  let validatorEmail = ""
-  validators.split(";").map((validator) => {
-    validatorEmail = fullNameToEmail(validator)
-    console.log("email index:", emailList.indexOf(validatorEmail))
-    if(emailList.indexOf(validatorEmail)) emailList.push(validatorEmail);
-    validatorsEmail += `${validatorEmail};`;
-  });
+  let validatorEmail = "";
+
+  if (validators.indexOf(";") !== -1) {
+    validators.split(";").map((validator) => {
+      validatorEmail = fullNameToEmail(validator);
+      //console.log("email index:", emailList.indexOf(validatorEmail));
+      if (emailList.indexOf(validatorEmail) === -1)
+        emailList.push(validatorEmail);
+      validatorsEmail += `${validatorEmail};`;
+    });
+  } else {
+    validatorsEmail = fullNameToEmail(validators);
+    emailList.push(validatorsEmail);
+  }
+
+  let duration =
+    differenceInBusinessDays(new Date(startDate), new Date(endDate)) + 1;
+  if (!startWholeDay) duration -= 0.5;
+  if (!endWholeDay) duration -= 0.5;
 
   // verify if the names are in our annuair.
   User.find({ email: { $in: emailList } }).then((users) => {
@@ -71,29 +91,26 @@ router.post("/send-request", (req, res, next) => {
         startWholeDay,
         endDate,
         endWholeDay,
-        requester: requesterEmail,
+        requestor: requestorEmail,
         validators: validatorsEmail,
         hr: "vbavoux@cicad.fr",
         sub: subEmail,
         comments,
       })
-        .then((request) => res.render("index"))
+        .then((request) => {
+          // send axios request here:
+          axios
+            .post(process.env.HTTPS_TRIGGER, request)
+            .then(() => res.render("index"))
+            .catch((err) => console.log("axios error:", err));
+        })
         .catch((err) => console.log("error in request creation", err));
     } else {
-      console.log("email list: ", emailList);
+      //console.log("email list: ", emailList);
       console.log("some emails do not exist");
-      res.render("index", { errorMessage: "Type all names correctly." });
+      res.render("index", { errorMessage: "Il faut bien vérifier les noms." });
     }
   });
-
-  let duration =
-    differenceInBusinessDays(new Date(startDate), new Date(endDate)) + 1;
-  if (!startWholeDay) duration -= 0.5;
-  if (!endWholeDay) duration -= 0.5;
-
-  // create request in the data base for log purpose
-
-  // send axios request here:
 });
 
 module.exports = router;
